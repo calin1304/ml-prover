6827c421ae969eaad9e1359cd8881ef1470a3739
Calin Nicolau <calin.nicolau@gmail.com>
2021-01-12 15:22:46 +0200

    Add unsafeLookup

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,4 +1,9 @@
 module Utils where
 
+import           Data.Maybe (fromJust)
+
 unsafeSplitHead :: [a] -> (a, [a])
 unsafeSplitHead (x:xs) = (x, xs)
+
+unsafeLookup :: Eq k => k -> [(k, v)] -> v
+unsafeLookup k = fromJust . lookup k
