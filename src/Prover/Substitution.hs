565b435a8978d029f71f73c3c7a4541fd651a5e8
Calin Nicolau <calin.nicolau@gmail.com>
2021-01-14 19:02:29 +0200

    Add substitution

--- src/Prover/Substitution.hs
+++ src/Prover/Substitution.hs
@@ -1,3 +1,15 @@
 module Prover.Substitution where
 
-subst = undefined
+import Data.Maybe (fromJust)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+
+import Language.Syntax
+
+newtype Subst = Subst (Map String SimpleExpr)
+
+applySubst :: Subst -> SimpleExpr -> SimpleExpr
+applySubst (Subst s) = \case
+    EVar name -> fromJust $ M.lookup name s
+    SVar name -> fromJust $ M.lookup name s
+    Application sym args -> Application sym (map (applySubst (Subst s)) args)
