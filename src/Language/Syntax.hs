c01385cc9e2b18717513c5c8e25a4239d1bc9b97
Calin Nicolau <calin.nicolau@gmail.com>
2021-05-19 16:12:37 +0300

    Add pretty printing for Syntax

--- src/Language/Syntax.hs
+++ src/Language/Syntax.hs
@@ -3,6 +3,8 @@ module Language.Syntax where
 import           Data.List       (intercalate)
 import           Test.QuickCheck
 import Text.Printf (printf)
+import qualified Text.PrettyPrint as PP
+import Text.PrettyPrint ((<+>))
 
 import           Language.Lexer
 
@@ -49,34 +51,13 @@ data Declaration =
     deriving (Eq)
 
 instance Show Declaration where
-    show = \case
-        MetaSym name attrs ->
-            printf "%s %s" name (show attrs)
-        Notation name sig def attrs ->
-            printf ("%s %s := %s %s") name (show sig) (show def) (show attrs)
-        Import name ->
-            name
-        Rule name args def ->
-            if null args
-                then printf "%s := %s" name (show def)
-                else
-                    printf
-                        "%s := forall %s : %s"
-                        name (intercalate " " args) (show def)
-        Lemma name args def proof ->
-            printf "_%s := forall %s : %s" name (intercalate " " args) (show def)
-
-class HasArgs a where
-    getArgs :: a -> [String]
-instance HasArgs Declaration where
-    getArgs (Rule _ args _) = args
-    getArgs _               = undefined
+    show = PP.render . docDeclaration
 
 class HasDefinition a where
     getDefinition :: a -> Expr
 instance HasDefinition Declaration where
-    getDefinition (Rule _ _ (FromDerive _ e)) = e
-    getDefinition _                           = undefined
+    getDefinition (Rule _ _ e) = e
+    getDefinition _ = undefined
 
 data Tactic =
     Intros String
@@ -94,14 +75,7 @@ data Expr =
 appPrec = 10
 
 instance Show Expr where
-    -- showsPrec d (Ident s) = showString s
-    -- showsPrec d (Application e1 e2) = showParen (d > appPrec)
-    --     $ showsPrec (d+1) e1 . showString " " . showsPrec (d+1) e2
-    -- showsPrec d (FromDerive pres e) = showsPrec d pres . showString " => " . showsPrec d e
-
-    show (Ident s) = s
-    show (Application e1 e2) = "(" ++ show e1 ++ ")" ++ " " ++ show e2
-    show (FromDerive pres e) = show pres ++ " |- " ++ show e
+    show = PP.render . docExpr
 
 -- I'm tired of writing long names
 infixl 5 ##
@@ -134,3 +108,43 @@ mkAttr name args =
         "set-binder"   -> SetBinder
         "notNegative"  -> NotNegative
         _              -> error $ "Invalid attribute name: " ++ name
+
+---------------------
+-- Pretty printing --
+---------------------
+
+docDeclaration :: Declaration -> PP.Doc
+docDeclaration = \case
+    MetaSym name _ -> PP.text name
+    Notation name _ def _ -> docDef name def
+    Import name -> PP.text name
+    Rule name args def ->
+        if null args
+            then docDef name def
+            else PP.hsep [PP.text name, definition, forall, PP.text (intercalate " " args), PP.char ':', docExpr def]
+    Lemma name args def proof ->
+        PP.hsep [PP.text name, definition, forall, PP.hsep $ map PP.text args, colon, docExpr def]
+
+docDef :: String -> Expr -> PP.Doc
+docDef name expr = PP.text name <> PP.text "≔" <> docExpr expr
+
+docExpr :: Expr -> PP.Doc
+docExpr = \case
+    Ident s -> PP.text s
+    Application e1 e2 -> PP.parens (docExpr e1) <> PP.space <> docExpr e2
+    FromDerive pres e ->
+        let docPres = if null pres then PP.empty else PP.text (show pres) -- TODO: doc pres
+         in PP.hsep [docPres, vdash, docExpr e]
+
+vdash :: PP.Doc
+vdash = PP.char '⊢'
+
+lambda = PP.char 'λ'
+-- let char_arrow ()  = if !Config.ascii then "->" else "→"
+-- let char_darrow () = if !Config.ascii then "=>" else "⇒"
+-- let char_prod ()   = if !Config.ascii then "forall" else "Π"
+forall = PP.char '∀'
+-- let char_equal ()  = if !Config.ascii then "==" else "≡"
+
+definition = PP.char '≔'
+colon = PP.char ':'
