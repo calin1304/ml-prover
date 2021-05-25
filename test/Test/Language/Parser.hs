9b8e9205793e18e16749caee33757eb4bd94aa01
Calin Nicolau <calin.nicolau@gmail.com>
2021-05-25 12:37:56 +0300

    Add more parser tests

--- test/Test/Language/Parser.hs
+++ test/Test/Language/Parser.hs
@@ -10,10 +10,14 @@ import Language.Parser
 import Language.ParserM
 
 tests :: TestTree
-tests = testGroup "Parser"
-    [ testCase "idParser" idParserTest
-    , testCase "applicationParser" applicationParserTest
-    ]
+tests =
+    testGroup "Parser"
+        [ testCase "idParser" idParserTest
+        , testCase "applicationParser" applicationParserTest
+        -- , testCase "metaSymParser" metaSymParserTest
+        -- , testCase "notationParser" notationParserTest
+        , testCase "ruleParser" ruleParserTest
+        ]
 
 idParserTest = actual @?= expected
   where
@@ -33,3 +37,55 @@ applicationParserTest = actual @?= expected
                 )
                 (Application (Application (Ident "g") (Ident "X")) (Ident "Y"))
     actual = runParserM parseExpression <$> scanner "(f X Y Z) (g X Y)"
+
+notationParserTest = actual @?= expected
+  where
+    expected =
+        Right
+            $ Notation
+                "nu"
+                (Signature [Argument "X", Argument "E"])
+                nuExpr
+                [Folded, Binder, NotNegative]
+    actual =
+        runParserM parseDeclaration
+            <$> scanner "notation nu (X : SetVar) E := not (mu X (not (#subst E X (not X)))) [folded, set-binder 1 2, notNegative]"
+
+    nuExpr =
+        Application (Ident "not")
+            ( Application
+                ( Application (Ident "mu") (Ident "X")
+                )
+                (
+                    ( Application (Ident "not")
+                        ( Application
+                            ( Application
+                                ( Application (Ident "#subst") (Ident "E")
+                                ) 
+                                (Ident "X")
+                            )
+                            ( Application (Ident "not") (Ident "X")
+                            )
+                        )
+                    )
+                )
+            )
+
+metaSymParserTest = actual @?= expected
+  where
+    expected = Right $ MetaSym "exists" [(Arity 2), Binder]
+    actual =
+        runParserM parseDeclaration <$> scanner "meta-symbol exists [arity 2, binder 1 2]"
+
+ruleParserTest = actual @?= expected
+  where
+    expected = Right $ Rule "mp" ["X", "Y"] mp
+    actual =
+        runParserM parseDeclaration
+            <$> scanner "rule mp X Y := from [X, impl X Y] derive Y"
+    mp =
+        FromDerive
+            [ Ident "X"
+            , Application (Application (Ident "impl") (Ident "X")) (Ident "Y")
+            ]
+            (Ident "Y")
