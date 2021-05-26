9d5bec89a6adba1e9916ec63c5b54a3e6be90f8a
Calin Nicolau <calin.nicolau@gmail.com>
2021-05-26 16:12:03 +0300

    Add ExceptT to InterpM

--- src/Interp.hs
+++ src/Interp.hs
@@ -1,25 +1,29 @@
 module Interp where
 
 import           Control.Monad.State
+import Control.Monad (unless)
 import           Debug.Trace
 import Data.Map (Map (..))
+import Control.Lens ((^.))
 import qualified Data.Map.Strict as M (fromList, union, insert)
+import Control.Monad.Except
 
 import           Language.Syntax
-import           Prover.ProofM       (ProofState (..), runProofM, step)
+import           Prover.ProofM       (ProofState (..), runProofM, step, _goal)
 import           Utils               (debugSection)
 
 type Env = Map String Declaration
 
-type InterpM = State Env
+type InterpError = String
+type InterpM = ExceptT InterpError (State Env)
 
-runInterpM :: InterpM a -> Env -> (a, Env)
-runInterpM = runState
+runInterpM :: InterpM a -> Env -> (Either InterpError a, Env)
+runInterpM = runState . runExceptT
 
 interp :: Declaration -> InterpM ()
 interp decl =
     case decl of
-        Rule name _ _ -> modify (M.insert name decl)
+        Rule name _ _ -> addDeclaration name decl
         Lemma name args (FromDerive premises goal) tactics -> do
             env <- get
             let
@@ -28,4 +32,13 @@ interp decl =
                     runProofM
                         (traverse step tactics)
                         (ProofState goal premises (M.union introArgs env))
-            seq (debugSection "Proof state" proofState') (pure ())
+            seq (debugSection "Proof state" proofState')
+                $ unless (isTop $ proofState' ^. _goal) (throwError "Could not prove top")
+        _ -> throwError "Invalid declaration"
+
+addDeclaration :: String -> Declaration -> InterpM ()
+addDeclaration name decl = modify (M.insert name decl)
+
+isTop :: Expr -> Bool
+isTop (Ident "top") = True
+isTop _ = False
